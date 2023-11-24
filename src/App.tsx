384ecca0353de1e52c3646caf31b995d4af7d86f
Louis Harris <lelightning@hotmail.fr>
2023-11-24 17:26:55 -0500

    mantine 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import { MantineProvider, useComputedColorScheme } from '@mantine/core';
-import { Notifications } from "@mantine/notifications";
-import { Routes, Route } from "react-router-dom";
-import BasePage from "./entry/BasePage";
-import DefaultPage from "./pages/default/Default";
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import BasePage from './entry/BasePage';
+import DefaultPage from './pages/default/Default';
 
 export default function App() {
-    const computedColorScheme = useComputedColorScheme('dark');
-    console.log("rendered");
+    const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+    console.log("App rendered");
     return (
-        <MantineProvider defaultColorScheme={computedColorScheme}>
-            <Notifications position="top-right" />
-            <BasePage title='CSM data'>
-                <Routes>
-                    <Route path='*' element={<DefaultPage />} /> {/* This is the default Route */}
-                </Routes>
-            </BasePage>
-        </MantineProvider>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+                <Notifications position="top-right" />
+                <BasePage title='Cortical Stimulation Tool'>
+                    <Routes>
+                        <Route path='*' element={<DefaultPage />} /> {/* This is the default Route */}
+                    </Routes>
+                </BasePage>
+            </MantineProvider>
+        </ColorSchemeProvider>
     );
 }
--- src/entry/BasePage.tsx
+++ src/entry/BasePage.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, AppShell, Group, Title, useMantineColorScheme } from '@mantine/core';
+import { ActionIcon, AppShell, Group, Header, Title, useMantineColorScheme } from '@mantine/core';
 import React, { PropsWithChildren } from 'react'
 import { IconSun, IconMoonStars } from '@tabler/icons-react';
 
@@ -8,31 +8,29 @@ export default function BasePage(props: PropsWithChildren<BasePageProps>) {
     return (
         <AppShell
             padding="md"
-            header={{ height: 60 }}
             styles={(theme) => ({
                 main: { backgroundColor: colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
             })}
-        >
-            <AppShell.Header>
-                <Group justify={'space-between'}>
-                    <Title>{props.title}</Title>
+            header={
+                <Header height={60}>
+                    <Group position="apart">
+                        <Title>{props.title}</Title>
 
-                    <Group justify='flex-end'>
-                        <ActionIcon
-                            variant="outline"
-                            color={dark ? 'yellow' : 'blue'}
-                            onClick={() => toggleColorScheme()}
-                            title="Toggle color scheme"
-                        >
-                            {dark ? <IconSun size="1rem" /> : <IconMoonStars size="1rem" />}
-                        </ActionIcon>
+                        <Group position="right">
+                            <ActionIcon
+                                variant="outline"
+                                color={dark ? 'yellow' : 'blue'}
+                                onClick={() => toggleColorScheme()}
+                                title="Toggle color scheme"
+                            >
+                                {dark ? <IconSun size="1rem" /> : <IconMoonStars size="1rem" />}
+                            </ActionIcon>
+                        </Group>
                     </Group>
-                </Group>
-            </AppShell.Header>
-
-            <AppShell.Main>
-                {props.children}
-            </AppShell.Main>
+                </Header>
+            }
+        >
+            {props.children}
         </AppShell>
     );
 }
