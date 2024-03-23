ad1cc965a53abcddf0a1df996cfa10a05ebdac8b
Louis Harris <lelightning@hotmail.fr>
2024-03-23 14:01:08 -0400

    added todos

--- src/pages/StimulationTool/tabs/t3_Summary.tsx
+++ src/pages/StimulationTool/tabs/t3_Summary.tsx
@@ -1,10 +1,19 @@
+import { DataTableSortStatus } from "mantine-datatable";
+import { useState } from "react";
 import { TabProperties } from "./tab_properties";
 
 // TODO: table with results summary
 // TODO: be able to filter table
+// TODO: sortable table
+// TODO: able to select what columns to show
 // TODO: be able to export table to csv / excel
 
 export default function SummaryTab({form, filters}: SummaryTabProps) {
+    //const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({ columnAccessor: 'id', direction: 'desc' });
+    const getRecordsFromForm = () => {
+        // Electrode, PointId, ROI, stimulation time, stimulation parameters ..., Task, Cognitive effect, Epi effect, EEG effect
+    }
+
     return (<>
         {filters?.pointIds}
     </>)
