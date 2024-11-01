cd7503f8c78b930a8d794de771f381ad83949358
Louis Harris <lelightning@hotmail.fr>
2024-10-31 20:18:09 -0400

    WIP trying to setup local emulators and dev config

--- src/infra/firebase/firebaseApp.ts
+++ src/infra/firebase/firebaseApp.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtsCxwNbAJCA8ooPz2IDHHbRv7uri5kOQ",
-  authDomain: "neuro-tools.firebaseapp.com",
-  projectId: "neuro-tools",
-  storageBucket: "neuro-tools.appspot.com",
-  messagingSenderId: "796322162018",
-  appId: "1:796322162018:web:657ebf09cfa11cf6fb783b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
