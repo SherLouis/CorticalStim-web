eb8891091747eda862e4fe19191de1e24dd09ab9
Louis Harris <lelightning@hotmail.fr>
2025-03-11 20:35:36 -0400

    fix colors for dark mode

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react"
 const Section = ({ header, children, bodyOpts, ...props }: SectionProps) => {
     const theme = useMantineTheme();
     const headerBgColor = theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[6];
-    const bodyBgColor = theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[4];
+    const bodyBgColor = theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[4];
     return (
         <Stack {...props} h={"100%"} w={"100%"} spacing={0}>
             <Box w={"100%"} px={theme.spacing.md}
